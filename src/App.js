18b5d60cf8ac0193365b31cb8c90bd663bc4ed44
haksuly1 <93102959+haksuly1@users.noreply.github.com>
2022-03-01 02:26:18 +0100

    changed welcome message

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        Welcome <code>To My meetApp</code> It will change the way you surf events.
         </p>
         <a
           className="App-link"
