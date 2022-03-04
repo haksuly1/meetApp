c14885be4fbd520763979d6d87209f96381eeb23
haksuly1 <93102959+haksuly1@users.noreply.github.com>
2022-03-04 01:16:15 +0100

    google domain verification

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        Welcome <code>To My meetApp</code> It will change the way you surf events.
+        Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
