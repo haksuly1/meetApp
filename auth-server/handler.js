0d869002c745bede1de9a9d0d112155d7271b42b
haksuly1 <93102959+haksuly1@users.noreply.github.com>
2022-03-16 02:52:27 +0100

    troubleshooting to allow all origin

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -42,13 +42,6 @@ const oAuth2Client = new google.auth.OAuth2(
  *
  */
 module.exports.getAuthURL = async () => {
-  /**
-   *
-   * Scopes array passed to the `scope` option. Any scopes passed must be enabled in the
-   * "OAuth consent screen" settings in your project on your Google Console. Also, any passed
-   *  scopes are the ones users will see when the consent screen is displayed to them.
-   *
-   */
   const authUrl = oAuth2Client.generateAuthUrl({
     access_type: "offline",
     scope: SCOPES,
@@ -64,8 +57,6 @@ module.exports.getAuthURL = async () => {
     }),
   };
 };
-
-
 
 module.exports.getAccessToken = async (event) => {
   // The values used to instantiate the OAuthClient are at the top of the file
