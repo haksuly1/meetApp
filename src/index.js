0470761b435103afd7c370d712a67803cd66f1eb
haksuly1 <93102959+haksuly1@users.noreply.github.com>
2022-03-29 06:56:46 +0200

    integrated atatus-js

--- src/index.js
+++ src/index.js
@@ -4,9 +4,11 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
-import * as atatus from 'atatus-spa';
+//import * as atatus from 'atatus-spa';
 
-atatus.config('AIzaSyAozvbNX9z2Or14N9x8DL-7d5Jw5qeR5hM').install();
+//atatus.config('AIzaSyAozvbNX9z2Or14N9x8DL-7d5Jw5qeR5hM').install();
+import * as atatus from 'atatus-js';
+atatus.config('81b6fba18bcb472eb25f4755396446b5').install();
 
 ReactDOM.render(
   <React.StrictMode>
@@ -24,3 +26,5 @@ serviceWorkerRegistration.unregister();
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+//atatus.notify(new Error('Test Atatus Setup'));
