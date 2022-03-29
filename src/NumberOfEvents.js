ca4e3a64379e99586cf8229535ff599e39476792
haksuly1 <93102959+haksuly1@users.noreply.github.com>
2022-03-29 11:57:55 +0200

    Implemented Alert via OOP to warn users and show errors

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,4 +1,48 @@
+import React, { Component } from 'react';
+import { ErrorAlert } from './Alert';
+
+class NumberOfEvents extends Component {
+
+  state = {
+    numberOfEvents: 32,
+    infoText: ''
+  }
+
+  handleInputChanged = (event) => {
+    const number = event.target.value;
+    if (number <= 0 || number > 32) {
+      this.setState({
+        numberOfEvents: number,
+        infoText: 'Choose number between 1 and 32'
+      });
+    } else {
+      this.setState({
+        numberOfEvents: number,
+        infoText: ''
+      });
+      this.props.updateNumberofEvents(number);
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        <p>Change number of shown events between 1-32
+        </p>
+
+        <input type="number" value={this.state.numberOfEvents} className="newValue" onChange={(e) => this.handleInputChanged(e)} />
+        <p><ErrorAlert text={this.state.infoText} /></p>
+      </div>
+    );
+  }
+}
+
+export default NumberOfEvents;
+
+/*
 import React, { Component } from "react";
+import { ErrorAlert } from './Alert';
+
 
 class NumberOfEvents extends Component {
   constructor() {
@@ -32,75 +76,5 @@ class NumberOfEvents extends Component {
 }
 
 export default NumberOfEvents;
-
-
-/*
-import React, { Component } from "react";
-
-class NumberOfEvents extends Component {
-  constructor() {
-    super();
-    // stting the values for the default states
-    this.state = {
-      numberOfEvents: 32,
-    };
-  }
-  
-  updateNumberOfEvents = (event) => {
-    this.setState({
-      numberOfEvents: event.target.value,
-    });
-    //this.props.updateEventNumbers(event.target.value);
-  };
-  */
-
-  /*
-  state = {
-    numberOfEvents: 32,
-    infoText: "",
-  };
-
-  handleInputChanged = (event) => {
-    const number = event.target.value;
-    if (value < 1 || value > 32) {
-      this.setState({
-        infoText: "Enter number between 1 and 32",
-      });
-    } else {
-      this.setState({
-        numberOfEvents: value,
-        infoText: "",
-      });
-    }
-    this.props.updateNumberOfEvents(event.target.value);
-  };
-
-  render() {
-    return (
-      <div className="NumberOfEvents">
-        <br></br>
-        <p>Number of Events:</p>
-        <input
-          type="number"
-          className="numberOfEvents"
-          value={this.state.numberOfEvents}
-          onChange={this.handleInputChanged}
-        />
-    
-  render() {
-    return (
-      <div className="numberOfEvents">
-        <label className="numberOfEvents__lable">Number of Events</label>
-        <input
-          type="number"
-          className="numberOfEvents__input"
-          value={this.state.numberOfEvents}
-          onChange={this.updateNumberOfEvents}>
-          </input>
-      </div>
-    );
-  }
-}
-
-export default NumberOfEvents;
 */
+
