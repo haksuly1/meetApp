20c5af2ed8a257fff9684267d421ccf0313badfb
haksuly1 <93102959+haksuly1@users.noreply.github.com>
2022-03-30 13:32:47 +0200

    fix: Auth set up

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -11,7 +11,7 @@ const credentials = {
   auth_uri: "https://accounts.google.com/o/oauth2/auth",
   token_uri: "https://oauth2.googleapis.com/token",
   auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-  redirect_uris: ["https://haksuly1.github.io/meetApp"],
+  redirect_uris: ["https://haksuly1.github.io/meetApp/"],
   javascript_origins: [
     "https://haksuly1.github.io", 
     "http://localhost:3000",
