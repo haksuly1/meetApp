eb1f29992942ab1b82e000c062556cbe7f0d392d
haksuly1 <93102959+haksuly1@users.noreply.github.com>
2022-04-03 08:13:16 +0200

    updated componentDidMount in App.js

--- src/App.js
+++ src/App.js
@@ -20,6 +20,22 @@ class App extends Component {
 
   componentDidMount() {
     this.mounted = true;
+
+    const accessToken = localStorage.getItem('access_token');
+    const isTokenValid = (await checkToken(accessToken)).error ? false :
+      true;
+    const searchParams = new URLSearchParams(window.location.search);
+
+    const code = searchParams.get("code");
+    this.setState({ showWelcomeScreen: !(code || isTokenValid) });
+    if ((code || isTokenValid) && this.mounted) {
+      getEvents().then((events) => {
+        if (this.mounted) {
+          this.setState({ events, locations: extractLocations(events) });
+        }
+      });
+    }
+  
     getEvents().then((events) => {
       if (this.mounted) {
         this.setState({
@@ -30,22 +46,8 @@ class App extends Component {
     });
   }
 
-  async componentWillUnmount() {
+  componentWillUnmount() {
     this.mounted = false;
-    this.mounted = true;
-    const accessToken = localStorage.getItem('access_token');
-    const isTokenValid = (await checkToken(accessToken)).error ? false :
-      true;
-    const searchParams = new URLSearchParams(window.location.search);
-    const code = searchParams.get("code");
-    this.setState({ showWelcomeScreen: !(code || isTokenValid) });
-    if ((code || isTokenValid) && this.mounted) {
-      getEvents().then((events) => {
-        if (this.mounted) {
-          this.setState({ events, locations: extractLocations(events) });
-        }
-      });
-    }
   }
 
   updateEvents = (location, eventCount = this.state.numberOfEvents) => {
