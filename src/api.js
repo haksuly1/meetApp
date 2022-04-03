f875da1c3c44c5b3cd3b16117599185c7abc8cf0
haksuly1 <93102959+haksuly1@users.noreply.github.com>
2022-04-03 08:34:19 +0200

    imported events to api.js

--- src/api.js
+++ src/api.js
@@ -1,6 +1,8 @@
+
 import { mockData } from './mock-data';
 import axios from "axios";
-import NProgress from "nprogress";
+import NProgress from 'nprogress';
+import events from './Events';
 
 export const extractLocations = (events) => {
   var extractLocations = events.map((event) => event.location);
