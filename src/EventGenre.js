37010f3e893807ffa5b11e2f5cb91c33559129cd
haksuly1 <93102959+haksuly1@users.noreply.github.com>
2022-04-04 09:24:10 +0200

    troubleshooting to display PieChart

--- src/EventGenre.js
+++ src/EventGenre.js
@@ -16,7 +16,7 @@ const EventGenre = ({ events }) => {
   const getData = () => {
     const genres = ["React", "JavaScript", "Node", "jQuery", "AngularJS"];
     const data = genres.map((genre) => {
-      const value = events.filter((event) => event.summary.split(", " ).includes(genre)).length;
+      const value = events.filter((event) => Event.summary.split(", " ).includes(genre)).length;
       return { name: genre, value: value };
     });
     return data;
