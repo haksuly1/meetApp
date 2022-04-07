496990468aede10b4aa0f1c1df17ff0eab67964d
haksuly1 <93102959+haksuly1@users.noreply.github.com>
2022-04-08 01:51:15 +0200

    edited EventGenere.js

--- src/EventGenre.js
+++ src/EventGenre.js
@@ -2,7 +2,25 @@ import React, { useEffect, useState } from "react";
 import { PieChart, Pie, Cell, ResponsiveContainer } from "recharts";
 
 const EventGenre = ({ events }) => {
-  const [data, setData] = useState([]);
+  const [ data, setData ] = useState([]);
+  
+  useEffect(() => {
+    const getData = () => {
+      const genres = ['React', 'JavaScript', 'Node', 'jQuery', 'AngularJS'];
+      const data = genres.map((genre) => {
+        const value = events.filter(({ summary }) =>
+          summary.includes(genre)
+        ).length;
+        return { name: genre, value };
+      });
+      return data.filter((genre) => { return genre.value > 0 });
+    };
+    setData(() => getData());
+  }, [events]);
+  
+  
+  
+  
   const colors = [
     "#d0427f", 
     "#f8a01f", 
@@ -11,16 +29,6 @@ const EventGenre = ({ events }) => {
     "#7dbeb8", 
     "#5c69a0"
   ];
-  useEffect(() => { setData(() => getData());}, [events]);
-
-  const getData = () => {
-    const genres = ["React", "JavaScript", "Node", "jQuery", "AngularJS"];
-    const data = genres.map((genre) => {
-      const value = events.filter((event) => event.summary.split(" " ).includes(genre)).length;
-      return { name: genre, value: value };
-    });
-    return data;
-  };
 
   return (
     <ResponsiveContainer height={400} >
